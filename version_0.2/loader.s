e3f68bef0d407c58cfe7fdb08aa151fa314d418b
EldenSokolaj <56181940+EldenSokolaj@users.noreply.github.com>
2020-08-14 15:09:32 -0500

    Create loader.s

---
+++ version_0.2/loader.s
@@ -0,0 +1,21 @@
+bits 32			;nasm directive - 32 bit
+section .text
+
+;multiboot spec
+    align 4
+    dd 0x1BADB002            ;magic
+    dd 0x00                  ;flags
+    dd - (0x1BADB002 + 0x00) ;checksum
+
+global start
+extern kmain	        ;kmain is defined in the cpp file
+
+start:
+  cli 			
+  mov esp, stack_space	;set stack pointer
+  call kmain
+  hlt		 
+
+section .bss
+resb 8192		;8KB for stack
+stack_space:
